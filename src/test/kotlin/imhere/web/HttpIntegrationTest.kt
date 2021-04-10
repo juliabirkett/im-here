3c88bd223bca92e3950f334749074ce72bb60059
juliabirkett <juliambirkett@gmail.com>
2021-04-10 16:26:46 +0100

    [x] Add failing test for POST /check-in

--- src/test/kotlin/imhere/web/HttpIntegrationTest.kt
+++ src/test/kotlin/imhere/web/HttpIntegrationTest.kt
@@ -1,11 +1,22 @@
 package imhere.web
 
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class HttpIntegrationTest {
+    private val handler = object : HttpHandler {
+        override fun invoke(p1: Request): Response {
+            TODO("Not yet implemented")
+        }
+    }
     @Test
-    fun firstTest() {
-        assertTrue(true)
+    fun `when checking in successfully, returns 201`() {
+        val request = Request(method = Method.POST, uri = "/check-in")
+
+        assertEquals(201, handler(request).status.code)
     }
 }
