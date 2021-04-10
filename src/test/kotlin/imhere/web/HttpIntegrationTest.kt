d13b47aa2cd971eb1dccd8bef169b4216ff8b0e1
juliabirkett <juliambirkett@gmail.com>
2021-04-10 16:30:38 +0100

    [✓] POST /check-in pass test

--- src/test/kotlin/imhere/web/HttpIntegrationTest.kt
+++ src/test/kotlin/imhere/web/HttpIntegrationTest.kt
@@ -1,16 +1,13 @@
 package imhere.web
 
-import org.http4k.core.HttpHandler
-import org.http4k.core.Method
-import org.http4k.core.Request
-import org.http4k.core.Response
+import org.http4k.core.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class HttpIntegrationTest {
     private val handler = object : HttpHandler {
         override fun invoke(p1: Request): Response {
-            TODO("Not yet implemented")
+            return Response(status = Status.CREATED)
         }
     }
     @Test
