824c05d53ff8b143d71e6bc481da226a9871ada7
juliabirkett <juliambirkett@gmail.com>
2021-04-10 15:40:12 +0100

    Initial Gradle bootstrap

---
+++ src/test/kotlin/imhere/web/HttpIntegrationTest.kt
@@ -0,0 +1,11 @@
+package imhere.web
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class HttpIntegrationTest {
+    @Test
+    fun firstTest() {
+        assertTrue(true)
+    }
+}
