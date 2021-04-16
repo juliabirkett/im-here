5de6735834136796b72a2f88f734238a0a583261
juliabirkett <juliambirkett@gmail.com>
2021-04-16 12:08:33 +0100

    Rename result failure to Error to improve readability

--- src/main/kotlin/errorhandling/Result.kt
+++ src/main/kotlin/errorhandling/Result.kt
@@ -6,35 +6,35 @@ sealed class Result<out T, out E> {
     companion object {
         fun <T> success(value: T): Result<T, Nothing> = Ok(value)
         fun <T> of(value: T): Result<T, Nothing> = Ok(value)
-        fun <E : ErrorCode> failure(err: E): Result<Nothing, E> = Err(err)
+        fun <E : ErrorCode> failure(err: E): Result<Nothing, E> = Error(err)
     }
 }
 class Ok<out T>(val value: T): Result<T, Nothing>()
-class Err<out E>(val value: E): Result<Nothing, E>()
+class Error<out E>(val value: E): Result<Nothing, E>()
 
 fun <U, T, E: ErrorCode> Result<T, E>.map(transform: (T) -> U): Result<U, E> = when (this) {
     is Ok -> Ok(transform(value))
-    is Err -> this
+    is Error -> this
 }
 
 fun <U, T, E: ErrorCode> Result<T, E>.mapError(transform: (E) -> U): Result<T, U> = when (this) {
     is Ok -> this
-    is Err -> Err(transform(value))
+    is Error -> Error(transform(value))
 }
 
 fun <U, T, E: ErrorCode> Result<T, E>.andThen(transform: (T) -> Result<U, E>): Result<U, E> = when (this) {
     is Ok -> transform(value)
-    is Err -> this
+    is Error -> this
 }
 
 fun <T> T.asSuccess(): Result<T, Nothing> = success(this)
 
 inline fun <T, E: ErrorCode> Result<T, E>.orElse(f: (E) -> T): T = when (this) {
-    is Err -> f(value)
+    is Error -> f(value)
     is Ok<T> -> value
 }
 
 fun <T> Result<T, ErrorCode>.success(): T? = when (this) {
     is Ok -> value
-    is Err -> null
+    is Error -> null
 }
