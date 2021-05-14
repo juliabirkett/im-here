298b9786b0bf4bf24b76ab25242d9d90dc339295
juliabirkett <juliambirkett@gmail.com>
2021-05-14 14:54:14 +0100

    Renaming for clarity

--- src/test/kotlin/imhere/functional/HttpApplicationTest.kt
+++ src/test/kotlin/imhere/functional/HttpApplicationTest.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class ApplicationTest {
+class HttpApplicationTest {
     private val handler = ImHereServer.handler
     private val server = handler.asServer(Netty())
     private val app: HttpHandler = ClientFilters.SetBaseUriFrom(Uri.of("http://localhost:${server.port()}")).then(handler)
